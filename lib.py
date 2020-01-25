2dc7ff5b5f03157253a047ccfa024fd862634a0a
Dani <55746309+23DP@users.noreply.github.com>
2020-01-25 14:52:01 +0100

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,10 +1,16 @@
-from functools import  reduce
-
-def sum(arr: list) -> int:
-    return reduce(lambda a,b: a+b, arr)
+def sum(arr: list) -> float:
+    sum = 0
+    for element in arr:
+        sum += element
+    return sum
 
 def diff(arr):
     retarr = []
     for i in range(len(arr) - 1):
         retarr.append(arr[i + 1] - arr[i])
     return retarr
+
+def PID(values: list, kp: float, ki: float, kd: float) -> float:
+    integral = sum(values[-4:])
+    derivative = sum(diff(values[-4:]))
+    return kp*values[-1] + ki*integral + kd*derivative
