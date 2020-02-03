3c7eeca40315938d69fceb3ef6a5c47dd5d538ee
Dani <55746309+23DP@users.noreply.github.com>
2020-02-03 21:13:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ left_motor = Motor(Port.B)
 color_sensor = ColorSensor(Port.S3)
 
 robot = DriveBase(right_motor, left_motor, 35, 144) #Initialization of a driving base
-target = 47
+target = 47 # return value from the reflection senscor when 50% on the line
 
 
 def drive_straight(driving_side: int, color: Color):
     error_history = [] # needed for PID
     steering_history = [] #neded for deviation
 
+    #until he reaches the given spot
     while color_sensor.color() != color : 
-        error_history.append( target - color_sensor.reflection() )
+        error_history.append( target - color_sensor.reflection() ) #append the error
         steering = lib.PID(error_history, 0.1, 0, 0.1)
         steering_history.append(steering)
         robot.drive(100, driving_side*steering)    
@@ -32,29 +33,30 @@ def drive_straight(driving_side: int, color: Color):
 
 
     robot.stop(stop_type = Stop.BRAKE)
-    return lib.sum( lib.diff(steering_history))    
+    return lib.sum( lib.diff(steering_history)) #Return the deviation from 0deg 
 
 
-color = [Color.GREEN, Color.RED, Color.BLUE]
+colors_list = [Color.GREEN, Color.RED, Color.BLUE]
 def getLogic(n: int):
     rotation = 1 if n < 4 else -1 #rotation direction
-    retColor = color[n%3]
+    retColor = colors_list[n%3] #this depends on your parking design
     return (rotation, retColor) 
-
+#TODO: move this to lib.py
 
 # main loop
 while not any(brick.buttons()):
-    os.system('clear')    
+    os.system('clear') # clearing the terminal for every iteration  
 
-    n = int( input("Broj parking mesta: ") )# waiting for a signal that there is a new car
+    n = int( input("Parking spot number: ") )# waiting for a signal that there is a new car
     rotation_side, color = getLogic(n)
+    
+    offset = drive_straight(1, color) # reach the depth of the parked car
 
-    offset = drive_straight(1, color)
+    # offset affects rotation differently depending on the side (clockwise or counter clockwise)
     rotation_angle = (90 + offset, 1000) if n > 3 else (90 - offset, 1000)
-    wait(20)
-    robot.drive_time(0, rotation_side*rotation_angle, 1000) #rotate
+    robot.drive_time(0, rotation_side*rotation_angle, 1000) #rotate to take a pic
  
-    paid = (input('Da li je placeno? [Y/n]') or 'y')
+    paid = (input('Confirm payment? [Y/n]') or 'y')
     paid = 1 if paid.lower() == 'y' else 0
     
     if paid:
@@ -62,7 +64,6 @@ while not any(brick.buttons()):
     else:
         brick.sound.file(SoundFile.UH_OH)
 
-    robot.drive_time(0, rotation_side*90, 1000) 
-
+    robot.drive_time(0, rotation_side*90, 1000) # go back to the resting spot
     _ = drive_straight(-1, Color.YELLOW)
-    robot.drive_time(0, 90, 2000) #90 degrees for 2 seconds = 180
+    robot.drive_time(0, 180, 1000) # Rotate to face the parking and rest until the next signal
